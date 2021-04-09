c7bc2a9b0c45f9fb6f9d777c2c84d8b775300a64
Juris Auzins <80879820+Juris-Auzinss@users.noreply.github.com>
2021-04-09 11:02:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 "use strict";
-const menuToggle = document.querySelector(".toggle");
-const showcase = document.querySelector(".showcase");
 
-menuToggle.addEventListener("click", () => {
-  menuToggle.classList.toggle("active");
-  showcase.classList.toggle("active");
-});
+const parallax = function (e) {
+  this.querySelectorAll(".layers").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+
+    const x = (window.innerWidth - e.pageX * +speed) / 1000;
+    const y = (window.innerHeight - e.pageY * +speed) / 1500;
+    layer.style.transform = `translateX(${x}rem) translateY(${y}rem)`;
+  });
+};
+
+document.addEventListener("mousemove", parallax);
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", (e) => {
+    // e.preventDefault();
+    //Toggle NAV
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.2
+        }s`;
+    });
+    //Burger Animation
+    burger.classList.toggle("burger-active");
+  });
+};
+navSlide();
